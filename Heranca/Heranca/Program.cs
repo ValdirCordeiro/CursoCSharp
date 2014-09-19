7b8ac2c8360b7757e6ec472d834d9f183d04e055
Valdir Júnior Cordeiro Rocha <vjcordeiror@gmail.com>
2014-09-19 20:56:19 -0300

    Módulo 7

---
+++ Heranca/Heranca/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Heranca
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           // Animal a = new Animal();
+            Cachorro a = new Cachorro();
+            a.Peso = 4;
+            a.Altura = 0.8;
+            
+
+            a.Mover();
+            a.MostrarDados();
+            a.Latir();
+        }
+    }
+}
---
+++ Object/Object/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Object
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Carro c = new Carro();
+            c.Marca = "Fiat";
+            c.Ano = 2010;
+            Console.WriteLine(c);
+
+            int x = 10;
+            string s = x.ToString();
+
+            string s2 = 10.ToString();
+
+            true.Equals(true);
+            21.3.Equals(10);
+        }
+    }
+
+    public class Carro
+    {
+        public string Marca { get; set; }
+        public int Ano { get; set; }
+
+        public override string ToString()
+        {
+            return Marca + " , " + Ano;
+        }
+    }
+}
