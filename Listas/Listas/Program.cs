08f3ab999c42eaed81f3c96de21b866d3a8b005c
Valdir Júnior Cordeiro Rocha <vjcordeiror@gmail.com>
2014-09-29 18:39:31 -0300

    Módulo 14

---
+++ Listas/Listas/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Listas
+{
+    class Program
+    {
+        static void Main()
+        {
+            List<string> nomes = new List<string>();
+            nomes.Add("José");
+            nomes.Add("Maria");
+            nomes.Add("Pedro");
+            nomes.Insert(1, "Paula");
+            
+            foreach(string nome in nomes)
+            {
+                Console.WriteLine(nome);
+            }
+
+            for(int i = 0; i < nomes.Count; i++)
+            {
+                Console.WriteLine(nomes[i]);
+            }
+
+            bool b = nomes.Contains("José");
+            Console.WriteLine(b);
+
+
+        }
+    }
+}
